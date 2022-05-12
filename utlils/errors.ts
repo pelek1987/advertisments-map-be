9e09e56ea959e91be89d72314980a82795b2d5c2
pelek1987 <pawel.sioda@gmail.com>
2022-05-12 22:03:53 +0200

    delete unnecessary semicolon

--- utlils/errors.ts
+++ utlils/errors.ts
@@ -1,6 +1,6 @@
 import {NextFunction, Request, Response} from "express";
 
-export class ValidationError extends Error {};
+export class ValidationError extends Error {}
 
 export const handleError = (err: Error, req: Request, res: Response, next:NextFunction) => {
     console.error(err);
