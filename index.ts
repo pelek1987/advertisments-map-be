afb59743395cab56eecc7710dbe352fb78fed4bd
pelek1987 <pawel.sioda@gmail.com>
2022-05-12 07:02:14 +0200

    add async errors handling

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, {json} from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import {handleError} from "./utlils/errors";
 
 const app = express();
 
@@ -8,6 +9,10 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }))
 app.use(json());
+
+// ...
+
+app.use(handleError)
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
