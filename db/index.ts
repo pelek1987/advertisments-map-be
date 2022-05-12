1451dc5d08f7120cdd5f560305ed476877c54deb
pelek1987 <pawel.sioda@gmail.com>
2022-05-12 22:20:30 +0200

    add environment variables for db connection

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,11 @@
+import 'dotenv/config';
 import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
     host: 'localhost',
     port: 3306,
-    database: 'advertisments_map',
-    user: 'root',
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: '',
     decimalNumbers: true,
     namedPlaceholders: true,
